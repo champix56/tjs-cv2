27f8f662c646e090ef833d8327b21d9679c311bf
alex <desorbaix@free.fr>
2020-06-16 10:13:52 +0200

    Banner de base

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
 import './App.css';
-
+import Banner from './components/Banner/Banner';
+import Navbar from './components/Navbar/Navbar';
+import Mainapp from './components/Mainapp/Mainapp';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-     
+     <Banner></Banner>
+     <Navbar></Navbar>
+     <Mainapp></Mainapp>
+     <Footer></Footer>
     </div>
   );
 }
--- src/App/components/Banner/Banner.js
+++ src/App/components/Banner/Banner.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Banner.module.css';
-
+import logo from '../../../logo.svg';
 const Banner = () => (
   <div className={styles.Banner} data-testid="Banner">
-    Banner Component
+            <img src={logo} className="App-logo" alt="logo" />
+            MEME <span className={styles.orange}>.editor</span>
   </div>
 );
 
