bf6ecd68858d1dc17aac4cc590e4eb772455e31b
alex <desorbaix@free.fr>
2020-06-20 02:47:51 +0200

    connexion au store et ecoute des  changements au cas ou un autre  composant change le contenu  de la liste du store ex ajout de meme par memeForm

--- src/App/components/MemesViewer/MemesViewer.js
+++ src/App/components/MemesViewer/MemesViewer.js
@@ -2,26 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './MemesViewer.module.css';
 import MemeViewer from '../MemeViewer/MemeViewer';
-
+import memeStore from '../../stores/MemeRedux';
 class MemesViewer extends React.Component {
-  constructor() {
-    super();
-    this.state={memes:[]}
+  constructor(props) {
+    super(props);
+    //ajout de props et super props
+    //notre etat initial sera la liste contenue dans l'etat du state
+    const storeState=memeStore.getState();
+    this.state={memes:storeState.memes}
+    //abonnement aux changement de l'etat 
+    //la recuperation du nouvelle etat se fait par store.getState
+    memeStore.subscribe(()=>this.setState({memes:memeStore.getState().memes}))
   }
-  componentDidMount(){
-    fetch('http://localhost:780/memes').then((r)=>{
-      return r.json()})
-      .then(r=>{
-      this.setState({memes:r});
-    })
-  }
+  
   render() {
-    console.log(this.state)
     return (
       <div className={styles.MemesViewer} data-testid="MemesViewer">
         {this.state.memes.map((e,i)=>{
-          console.log(e);
-          return <MemeViewer meme={e}
+          return <MemeViewer memeId={e.id}
             key={`meme-${i}`}
           />})}
       </div>
